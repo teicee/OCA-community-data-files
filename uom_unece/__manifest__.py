17c4ebb40c536d5166fcbccbf3706a8037ea1ea8
Alexis de Lattre <alexis.delattre@akretion.com>
2023-02-10 00:58:29 +0100

    uom_unece: add UNECE code for yard
    Inherit form view of uom.category to add unece code in the embedded tree view of uom.uom.

--- uom_unece/__manifest__.py
+++ uom_unece/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Product UoM UNECE",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Sales",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
@@ -13,6 +13,6 @@
     "maintainers": ["astirpe", "alexis-via"],
     "website": "https://github.com/OCA/community-data-files",
     "depends": ["uom"],
-    "data": ["data/unece.xml", "views/uom_uom.xml"],
+    "data": ["data/unece.xml", "views/uom_uom.xml", "views/uom_category.xml"],
     "installable": True,
 }
